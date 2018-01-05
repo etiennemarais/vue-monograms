6ecec5a6b4e05839c23ba8cc45cd1859bc958714
Etienne Marais <etienne.m@superbalist.com>
2018-01-05 15:44:01 +0200

    Make more public friendly package details

---
+++ index.js
@@ -0,0 +1,2 @@
+import plugin from './dist/vue-monogram';
+export default plugin
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 let mix = require('laravel-mix').mix;
 
 mix.js('src/js/index.js', 'docs/js')
+    .js('src/js/components/Monogram', 'dist/vue-monograms.js')
     .copy('src/index.html', 'docs/index.html')
     .sass('src/scss/app.scss', 'docs/css')
     .disableNotifications();
