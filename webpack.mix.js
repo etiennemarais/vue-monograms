d10b15fde3e0ad1aa3c017aa377de043ddd5d560
Etienne Marais <etienne.m@superbalist.com>
2018-01-05 15:12:39 +0200

    Added cdn for fonts rather than struggle with this local stuff

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,5 +3,4 @@ let mix = require('laravel-mix').mix;
 mix.js('src/js/index.js', 'docs/js')
     .copy('src/index.html', 'docs/index.html')
     .sass('src/scss/app.scss', 'docs/css')
-    .copy('./fonts', 'docs/fonts')
     .disableNotifications();
